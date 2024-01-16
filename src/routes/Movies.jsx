82ecc6fa16b2bef4bb9bca64d1256f9d76455add
etoilebonbon <violetjardin@daum.net>
2024-01-16 11:16:06 +0900

    20240116

--- src/routes/Movies.jsx
+++ src/routes/Movies.jsx
@@ -1,24 +1,73 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../components/Layout'
+import CircularProgress from '../components/CircularProgress'
+import Pagination from "react-js-pagination";
+import "./Paging.css"
 
 export default function Movies() {
+  const [lists, setLists] = useState()
+  const [page, setPage] = useState(1);
+
+  // console.log(lists.total_pages) 토탈페이지 확인
+
+  useEffect(()=>{
+    const url = `https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1${page}`;
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNmMwM2Q1ZWMxMmUzZTBhZjFmMGE5ZDI4NGNjYWVkOCIsInN1YiI6IjY1OWRmNWMwMWQzNTYzMDBlZWU5NDIxMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YrTanxjWZbWXMLQWgSPcDb3oM6vVgTsEVlkpACS-zqA'
+      }
+    };
+
+    fetch(url, options)
+      .then(res => res.json())
+      .then(json => {
+        console.log(json)
+        setLists(json)
+      })
+      .catch(err => console.error('error:' + err));
+      }, [page])
+    
+      const handlePageChange = (page) => {
+        setPage(page)
+      }
+      
   return (
     <>
       <Layout>
-       <div className="w-full flex justify-center py-16">
-        <div className="w-[1000px] ">
+       <div className="w-full flex flex-col items-center justify-center py-16">
+        <div className="w-[1000px] flex flex-wrap gap-4 gap-y-8">
           {/* item */}
-          <div className="w-[180px] h-[340px] rounded-lg shadow-lg overflow-hidden">
+          {lists?.results?.map((list)=>(
+            <div key = {list.id} className="w-[180px] h-[340px] rounded-lg shadow-lg overflow-hidden">
             {/* 위: 그림 */}
             <div className="w-full h-[250px] ">
-              <img className="w-full h-full object-cover" src = "https://plus.unsplash.com/premium_photo-1664640458482-23df72d8b882?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+              <img className="w-full h-full object-cover" 
+              src = {`https://image.tmdb.org/t/p/w500${list.poster_path}`} />
             </div>
             {/* 아래: 내용 */}
-            <div className="w-full h-[90px] ">
-              <h2>타이틀</h2>
-              <p>2024-01-16</p>
+            <div className="relative w-full h-[90px] pt-6 px-2 ">
+              <h2 className="font-semibold">{list.title.substr(1, 20)}</h2>
+              <p className="text-sm">{list.release_date}</p>
+              {/* 좋아요 평가 */}
+              <div className="absolute -top-5 left-2 ">
+                <CircularProgress rate={Math.floor(list.vote_average*10)} />
+              </div>
             </div>
           </div>
+          ))}
+          
+        </div>
+        {/* 페이지 네이션 */}
+          <div className="pt-8">
+          <Pagination
+            activePage={page}
+            itemsCountPerPage={10}
+            totalItemsCount={lists?.total_pages}
+            pageRangeDisplayed={5}
+            onChange={handlePageChange}
+          />
         </div>
        </div>
       </Layout>
